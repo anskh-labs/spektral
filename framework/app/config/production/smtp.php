a62683247d290c1a17a3ceefd7ae16fbda7dcc8c
Khaerul Anas <khaerulanas@live.com>
2025-05-08 10:19:24 +0700

    Update smtp.php

--- framework/app/config/production/smtp.php
+++ framework/app/config/production/smtp.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'host' => 'smtp.bps.go.id',
     'port' => 587, //SMTPS 465, TLS 587
-    'user' => 'ipds1400',
-    'pass' => '1pd51400',
+    'user' => '',
+    'pass' => '',
 ];
